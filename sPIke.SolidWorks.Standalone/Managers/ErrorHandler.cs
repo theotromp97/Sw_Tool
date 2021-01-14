6b9fb775357d9fb655eca92e5b265e783dc8b1ba
Theo Tromp <theotromp97@gmail.com>
2021-01-14 22:44:05 +0100

    added content to error handler

--- sPIke.SolidWorks.Standalone/Managers/ErrorHandler.cs
+++ sPIke.SolidWorks.Standalone/Managers/ErrorHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace sPIke.SolidWorks.Standalone
 {
@@ -12,6 +13,35 @@ namespace sPIke.SolidWorks.Standalone
         public ErrorHandler(ClassManager _classManager)
         {
             classManager = _classManager;
+        }
+
+
+        public static void errorMessageHanding(int errorMessage)
+        {
+            if (errorMessage == 1)
+            {
+                MessageBox.Show("No project selected or the right extensions is not selected. Meaning that there is no possibility for creating a part, assembly or drawing.", "NO PROJECT/EXTENSION FOUND", 0, MessageBoxIcon.Stop);
+            }
+            else if (errorMessage == 2)
+            {
+                MessageBox.Show("You did not enter your name.", "NO AUTHOR NAME FOUND", 0, MessageBoxIcon.Stop);
+            }
+            else if (errorMessage == 3)
+            {
+                MessageBox.Show("There is no instance of SolidWorks available for usage.", "NO INSTANCE OF SOLIDWORKS FOUND", 0, MessageBoxIcon.Stop);
+            }
+            else if (errorMessage == 4)
+            {
+                MessageBox.Show("The google folder is found so no further action is required to connect the program to the project folder list.", "PROJECT FOLDER FOUND", 0, MessageBoxIcon.Information);
+            }
+            else if (errorMessage == 5)
+            {
+                MessageBox.Show("The google folder is NOT found so a manual action is required to connect the program to the project folder list.", "PROJECT FOLDER NOT FOUND", 0, MessageBoxIcon.Stop);
+            }
+            else if (errorMessage == 6)
+            {
+                MessageBox.Show("There is no folder selected so the action has failed.", "PROJECT FOLDER NOT SELECTED", 0, MessageBoxIcon.Stop);
+            }
         }
     }
 }
