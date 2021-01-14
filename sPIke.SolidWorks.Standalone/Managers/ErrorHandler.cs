a69b4a2dc9d63876accbe2494c3583fc0eaa3b01
Theo Tromp <theotromp97@gmail.com>
2021-01-14 22:38:43 +0100

    added errorhandler class

---
+++ sPIke.SolidWorks.Standalone/Managers/ErrorHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sPIke.SolidWorks.Standalone
+{
+    public class ErrorHandler
+    {
+        public ClassManager classManager;
+        public ErrorHandler(ClassManager _classManager)
+        {
+            classManager = _classManager;
+        }
+    }
+}
